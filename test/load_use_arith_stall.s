7594a7ff030747791355a51586025eb704fa079b
Ammaar Alam <ammaar@princeton.edu>
2025-12-11 14:34:01 -0500

    updating loadbranch cases

--- test/load_use_arith_stall.s
+++ test/load_use_arith_stall.s
@@ -1,11 +1,9 @@
 _start:
-    # Load -> store-data fowrading test
-    # sw immediately after lw shouldnt cause a load-use stall
-    li   t0, 256
-    li   t1, 123
+    # simple load-use stall (1 cycle) w/ arithmetic consumer
+    # should be one bubble between lw and add, and load-use stalls stat +1
+    li   t0, 300
+    li   t1, 7
     sw   t1, 0(t0)
     lw   t2, 0(t0)
-    sw   t2, 4(t0)      # should get WB->MEM forwarded data, no bubble
-    lw   t3, 4(t0)
+    add  t3, t2, t2
     .word 0xfeedfeed
-
